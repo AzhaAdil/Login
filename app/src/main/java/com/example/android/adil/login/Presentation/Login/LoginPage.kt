66c3399593f01eb068de9aca4c749292dbb4088b
Adil <azhaaadil787@gmail.com>
2022-11-11 23:09:48 -0800

    this is my first our final

--- app/src/main/java/com/example/android/adil/login/Presentation/Login/LoginPage.kt
+++ app/src/main/java/com/example/android/adil/login/Presentation/Login/LoginPage.kt
@@ -2,25 +2,53 @@ package com.example.android.adil.login.Presentation.Login
 
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
+import android.text.TextUtils
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.EditText
+import android.widget.Toast
+import androidx.navigation.Navigation
+import androidx.navigation.Navigator
+import androidx.navigation.fragment.findNavController
 import com.example.android.adil.login.R
+import com.example.android.adil.login.databinding.ActivityMainBinding
+import com.example.android.adil.login.databinding.FragmentLoginPageBinding
 
 class LoginPage : Fragment() {
 
 
-
+lateinit var binding: FragmentLoginPageBinding
     private lateinit var viewModel: LoginPageViewModel
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         viewModel = ViewModelProvider(this).get(LoginPageViewModel::class.java)
-        val view:View= inflater.inflate(R.layout.fragment_login_page, container, false)
+        binding=FragmentLoginPageBinding.inflate(inflater)
 
-        return  view
+
+        binding.Login.setOnClickListener {
+            val email=binding.email
+            val password=binding.password
+            when{
+
+                TextUtils.isEmpty(email.text.toString())-> Toast.makeText(context,"email",Toast.LENGTH_SHORT).show()
+                TextUtils.isEmpty(password.text.toString())->Toast.makeText(context,"password",Toast.LENGTH_SHORT).show()
+                else->{
+                    viewModel.Login(email.text.toString(),password.text.toString())
+                    Toast.makeText(context,"your are Logged",Toast.LENGTH_SHORT).show()
+
+                }
+            }
+        }
+        binding.Create.setOnClickListener {
+         findNavController().navigate(R.id.action_loginPage_to_signUp)
+        }
+
+
+
+       return binding.root
     }
 
 
--- app/src/main/java/com/example/android/adil/login/Presentation/Sign_up/SignUp.kt
+++ app/src/main/java/com/example/android/adil/login/Presentation/Sign_up/SignUp.kt
@@ -2,18 +2,19 @@ package com.example.android.adil.login.Presentation.Sign_up
 
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
+import android.text.TextUtils
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.navigation.fragment.findNavController
 import com.example.android.adil.login.R
+import com.example.android.adil.login.databinding.FragmentSignUpBinding
 
 class SignUp : Fragment() {
 
-    companion object {
-        fun newInstance() = SignUp()
-    }
-
+    lateinit var binding: FragmentSignUpBinding
     private lateinit var viewModel: SignUpViewModel
 
     override fun onCreateView(
@@ -21,8 +22,28 @@ class SignUp : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         viewModel = ViewModelProvider(this).get(SignUpViewModel::class.java)
-        return inflater.inflate(R.layout.fragment_sign_up, container, false)
+        binding=FragmentSignUpBinding.inflate(inflater)
 
+        val username=binding.username
+        val email=binding.email
+        val password=binding.password
+        binding.Registor.setOnClickListener {
+            when{
+                TextUtils.isEmpty(username.text.toString())->Toast.makeText(context,"username",Toast.LENGTH_SHORT).show()
+                TextUtils.isEmpty(email.text.toString())-> Toast.makeText(context,"email", Toast.LENGTH_SHORT).show()
+                TextUtils.isEmpty(password.text.toString())-> Toast.makeText(context,"password", Toast.LENGTH_SHORT).show()
+                else->{
+                    viewModel.SignUp(username.text.toString(),email.text.toString(),password.text.toString())
+                    Toast.makeText(context,"your are Logged", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+
+        binding.Already.setOnClickListener {
+            findNavController().navigate(R.id.action_signUp_to_loginPage)
+        }
+        binding.root
+        return binding.root
     }
 
 
